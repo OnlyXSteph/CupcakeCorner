8832f3ef6492ca5e035e7a998cd293e4ad1beabf
Stephanie <83602099+OnlyXSteph@users.noreply.github.com>
2024-05-29 14:52:17 -0500

    Add files via upload

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,18 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Stephanie Salgado on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
